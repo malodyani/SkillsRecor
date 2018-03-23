2f5285b616de992098875ce477c1928d48ed94f9
Mohammed <m.alodyani@gmail.com>
2018-03-23 05:07:11 +0300

    edit slider page

--- resources/views/login/slider.blade.php
+++ resources/views/login/slider.blade.php
@@ -1,16 +1,12 @@
 	<nav class="">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-   
-    <!-- Collect the nav links, forms, and other content for toggling -->
-   
-  </div><!-- /.container-fluid -->
+
+  </div>
 
             <ul class="nav navbar-top-links navbar-right text-slider">
                
               
-                <!-- /.dropdown -->
-                
+            
                 <li class="dropdown" style="margin-right:170px;">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
@@ -22,46 +18,56 @@
                         <li><a href="loguot"><i class="fa fa-sign-out fa-fw"></i>تسجيل الخروج</a>
                         </li>
                     </ul>
-                    <!-- /.dropdown-user -->
+                  
                 </li>
-                <!-- /.dropdown -->
+              
             </ul>
-            <!-- /.navbar-top-links -->
+        
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
+                       <!--Start for student-->
+                        <li>
+                            <a  href="/Home"><i class="fa fa-dashboard fa-fw"></i> الرئيسية</a>
+                        </li>
+                        <li>
+                            <a href="AddCourse"><i class="fa fa-table fa-fw"></i>اضافة دورات </a>
+                        </li>
+                        <li>
+                            <a href="AddAward"><i class="fa fa-edit fa-fw"></i> اضافة جوائز</a>
+                        </li>
+                        <li>
+                            <a href="AddActivity"><i class="fa fa-edit fa-fw"></i> اضافة انشطة</a>
+                        </li>
+                         <!--end for student-->
+                         
+                        <!--Start for employees-->
+                        <li>
+                            <a  href="/search"><i class="fa fa-dashboard fa-fw"></i> الرئيسية</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-table fa-fw"></i>الاحصائيات</a>
+                        </li>
+                       
+                        <!--end for employees-->
+
+
+                        <!--start for admin-->
                        
                         <li>
-                            <a class="active" href="/Home"><i class="fa fa-dashboard fa-fw"></i> الرئيسية</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> السجل المهاري<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">test</a>
-                                </li>
-                                <li>
-                                    <a href="#">test</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-table fa-fw"></i> الدورات</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-edit fa-fw"></i> الجوائز</a>
-                        </li>
-                      
-                        <li>
-                            <a href="content"><i class="fa fa-edit fa-fw"></i> ادارة المحتوى</a>
+                            <a  href="content"><i class="fa fa-dashboard fa-fw"></i> الرئيسية</a>
                         </li>
 
+                        <li>
+                            <a href="#"><i class="fa fa-table fa-fw"></i>الاحصائيات</a>
+                        </li>
+                     
+                        <!--end for admin-->
                           </li>
                     </ul>
                 </div>
-                <!-- /.sidebar-collapse -->
+              
             </div>
-            <!-- /.navbar-static-side -->
+          
         </nav>
         <div id="page-wrapper" style=" background-image: url('http://www.su.edu.sa/_LAYOUTS/15/New_SUPortal/images/ar/new/bg_body.png')" >
